4cee40a2068d161750c5472bb7339627c13779b3
veileddraconis <veileddraconis@live.com>
2016-01-20 12:24:51 -0500

    First commit

---
+++ src/me/veileddraconis/MyArena/MyArena.java
@@ -0,0 +1,9 @@
+package me.veileddraconis.MyArena;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Draconis on 1/20/2016.
+ */
+public class MyArena extends JavaPlugin {
+}
---
+++ src/me/veileddraconis/controllers/PlayerController.java
@@ -0,0 +1,7 @@
+package me.veileddraconis.controllers;
+
+/**
+ * Created by Draconis on 1/20/2016.
+ */
+public class PlayerController {
+}
